0ee092f98e5c70866cf75437afd68205489a4834
SeunginLyu <seungin.lyu@students.olin.edu>
2018-09-18 11:21:23 -0400

    Updating links to jekyll links (prepend root)

---
+++ assets/js/toc.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.toc-entry').click(function(){
+       $('#toggle').attr('checked', false);
+        console.log("clicked");
+    })
+    $('.toc-entry a').click(function(){
+       $('#toggle').attr('checked', false);
+    })
+    $('.anchor').parent().hover(function(){
+        if($(window).width() > 1024){
+        $(this).find('.anchor > span.octicon').show();
+        }
+
+    }, function(){
+        $(this).find('.anchor > span.octicon').hide();
+    });
+    var scrollTime= 500;
+    $('a[href^="#"]').click(function() {
+        // Need both `html` and `body` for full browser support
+        var offset=- 55;
+        if($(window).width() > 1024){
+            offset= 0;
+        }
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href") ).offset().top + offset
+         }, scrollTime);
+         // Prevent the jump/flash
+        return false
+    });
+});
